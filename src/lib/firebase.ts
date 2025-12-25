3b159b07832ac9341d9944e97901f837f99870be
Devi sri prasad nakka <nakkdevisriprasad@gmail.com>
2025-12-25 17:16:38 +0530

    fix: Use environment variables for Firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,15 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDKjAvinkm_l2VeX-AbzmkaYBWrhPiTMUxo",
-    authDomain: "galaxleaf.firebaseapp.com",
-    projectId: "galaxleaf",
-    storageBucket: "galaxleaf.firebasestorage.app",
-    messagingSenderId: "272784789246",
-    appId: "1:272784789246:web:528abdaedbf7a6c6b11faf",
-    measurementId: "G-VRLPC6HYY5"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase (SSR Safe)
